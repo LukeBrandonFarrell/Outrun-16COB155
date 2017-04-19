7fb6adfe19dd4c766c8ccc6e0e6bde1e4386cb26
PadWorkGames <support@padworkgames.com>
2017-04-19 17:35:03 +0200

    Initial commit

---
+++ app/src/main/java/com/lborocoursework/outrun/HistoryActivity.java
@@ -0,0 +1,11 @@
+package com.lborocoursework.outrun;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class HistoryActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.history_layout);
+    }
+}
